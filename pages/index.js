9713a7769f2011410e1201b97c5330948bf4435d
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2021-12-11 01:21:44 -0500

    Index: Add sub containers

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,73 @@
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+import Image from "next/image";
+
+function Container() {
+  return (
+    <div className="grid grid-cols-4 gap-y-96 p-8">
+      <FirstContainer />
+      <SecondContainer />
+      <ThirdContainer />
+      <FourthContainer />
+      <FifthContainer />
+    </div>
+  );
+}
+
+function FirstContainer() {
+  return (
+    <div className="col-span-4">
+      <h1 className="text-6xl text-center font-bold text-gray-800">How Pandemic Changed Music?</h1>
+    </div>
+  );
+}
+
+function SecondContainer() {
+  return (
+    <div className="col-span-4">
+      <h1 className="text-6xl text-center font-bold text-gray-800">
+        How to find emotions programmatically?
+      </h1>
+    </div>
+  );
+}
+
+function ThirdContainer() {
+  return (
+    <div className="col-span-4">
+      <h1 className="text-6xl text-center font-bold text-gray-800">
+        Emotion of all songs from 2017 to 2021.
+      </h1>
+    </div>
+  );
+}
+
+function FourthContainer() {
+  return (
+    <div className="col-span-4">
+      <h1 className="text-6xl text-center font-bold text-gray-800">
+        Pre and post-pandemic emotions.
+      </h1>
+    </div>
+  );
+}
+
+function FifthContainer() {
+  return (
+    <div className="col-span-4">
+      <h1 className="text-6xl text-center font-bold text-gray-800">Conclusion</h1>
+    </div>
+  );
+}
+
+export default Container;
+
+{
+  /* <div className="hidden max-w-md sm:max-w-full mt-16">
+  <div className="h-60 w-full sm:object-cover relative rounded-lg shadow-lg">
+    <Image alt="placeholder" layout="fill" src="https://via.placeholder.com/500" />
+  </div>
+  <p className="mt-8">
+    We often associate music with emotions. For example, Uptown Funk is a happy song, Scientist by
+    ColdPlay is a sad song.
+  </p>
+</div>; */
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
+
+// theme: {
+//   fontFamily: {
+//     display: ["Gilroy", "sans-serif"],
+//     body: ["Graphik", "sans-serif"],
+//   },
+//   extend: {},
+// },
