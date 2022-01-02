83db83e5502acc257653e87dd940d9cfb35cf4f9
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2022-01-02 15:43:40 -0500

    Tailwid: Add custom fonts

--- components/HomePage/TitleContainer.js
+++ components/HomePage/TitleContainer.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function TitleContainer() {
   return (
-    <div className="w-full p-8 flex flex-wrap flex-row">
+    <div className="w-full py-20 flex flex-wrap flex-row">
       <div className="w-2/3 self-center">
         <Image
           src="/MusicThroughThePandemic.svg"
@@ -11,12 +11,15 @@ function TitleContainer() {
           height={491}
         />
       </div>
-      <div className="font-serif italic self-center px-8">
-        <span className="text-orange-500 font-bold">Sparsh</span> Paliwal
+      <div className="w-1/3 flex flex-wrap flex-col-reverse font-serif italic text-right text-2xl">
+        <div className="pb-2">
+          <span className="text-orange-500 font-bold">Sparsh</span> Paliwal
+        </div>
+        <div>
+          <span className="text-orange-500 font-bold">Eun</span> Jegal
+        </div>
       </div>
-      <div className="font-serif italic self-center">
-        <span className="text-orange-500 font-bold">Eun</span> Jegal
-      </div>
+      <div className="absolute bg-orange-500 h-96 w-96"></div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      serif: ["Merriweather", ...defaultTheme.fontFamily.serif],
+      mono: [...defaultTheme.fontFamily.mono],
+    },
     extend: {},
   },
   plugins: [],
 };
-
-// theme: {
-//   fontFamily: {
-//     display: ["Gilroy", "sans-serif"],
-//     body: ["Graphik", "sans-serif"],
-//   },
-//   extend: {},
-// },
