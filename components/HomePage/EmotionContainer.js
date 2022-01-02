12fcc6db6366d777baa7eb60f72b3bdda269856d
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2022-01-02 18:47:21 -0500

    Homepage: Fix styling for Did Pandemic Change Music Section

--- components/HomePage/EmotionContainer.js
+++ components/HomePage/EmotionContainer.js
@@ -1,24 +1,8 @@
 import Image from "next/image";
 
-function SongCoverAndComment(props) {
-  return (
-    <>
-      <div className="w-fit rounded-full py-3 px-6 bg-[#191F40]">
-        {props.comment} <span className={`${props.emotionColor} font-bold`}>{props.emotion}.</span>
-      </div>
-      <div className="w-full relative left-14">
-        <Image src="/bubblePointer.svg" alt="" width={14.5} height={24} />
-      </div>
-      <div className="w-full">
-        <Image src={props.songCoverUrl} alt={props.songCoverAltText} width={240} height={240} />
-      </div>
-    </>
-  );
-}
-
 function EmotionContainer() {
   return (
-    <div className="w-full pt-36 flex flex-wrap flex-col">
+    <div className="w-full mt-36 flex flex-wrap flex-col">
       <h1 className="w-full text-4xl font-serif italic">Music and Emotion</h1>
       <div className="w-full pt-2">
         We often<span className="text-orange-500 font-bold"> refer music with the emotion</span> it
@@ -54,6 +38,22 @@ function EmotionContainer() {
         </div>
       </div>
     </div>
+  );
+}
+
+function SongCoverAndComment(props) {
+  return (
+    <>
+      <div className="w-fit rounded-full py-3 px-6 bg-[#191F40]">
+        {props.comment} <span className={`${props.emotionColor} font-bold`}>{props.emotion}.</span>
+      </div>
+      <div className="w-full relative left-14">
+        <Image src="/bubblePointer.svg" alt="" width={14.5} height={24} />
+      </div>
+      <div className="w-full">
+        <Image src={props.songCoverUrl} alt={props.songCoverAltText} width={240} height={240} />
+      </div>
+    </>
   );
 }
 
--- components/HomePage/PandemicContainer.js
+++ components/HomePage/PandemicContainer.js
@@ -1,29 +1,43 @@
+import Image from "next/image";
+const COVID_IMAGE_IN_BUS =
+  "https://images.unsplash.com/photo-1610988760355-42a24e842cdf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80";
+const COVID_IMAGE_OUTDOORS =
+  "https://images.unsplash.com/photo-1585175170532-fbd7792dec54?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2669&q=80";
+
 function PandemicContainer() {
   return (
-    <div className="pt-40 w-full flex flex-wrap flex-row">
+    <div className="mt-40 w-full flex flex-wrap flex-row">
       <div className="w-3/5">
-        <h1 className="text-4xl font-serif italic">Did Pandemic Change Music?</h1>
-        <div className="pb-2 pt-4">
+        <h2 className="text-4xl font-serif italic">Did Pandemic Change Music?</h2>
+        <div className="pb-2 pt-4 pr-10">
           Pandemic turned our world upside down. People were full of fear and anxiety due to the
           uncertainty of future. Further, many struggled adapting to the new nature of things such
           as work and education.
         </div>
         <div className="pb-2">
-          We wanted to
+          We wanted to{" "}
           <span className="text-orange-500 font-bold">
-            investigate the emotional patterns of music before and after pandemic.
+            investigate the emotional patterns of music before and after pandemic.{" "}
           </span>
           Our hypothesis was that people would have listened to sadder music during pandemic.
         </div>
       </div>
-      <img
-        src="https://images.unsplash.com/photo-1610988760355-42a24e842cdf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=776&q=80"
-        className="w-2/5 h-96 object-cover rounded-2xl"
-      />
-      <img
-        src="https://images.unsplash.com/photo-1585175170532-fbd7792dec54?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2669&q=80"
-        className="w-full h-96 object-cover rounded-2xl"
-      />
+      <div className="w-2/5 h-56 relative">
+        <Image
+          src={COVID_IMAGE_IN_BUS}
+          alt="Women wearing a mask and walking on the street."
+          layout="fill"
+          className="object-cover object-center rounded-2xl top-10"
+        />
+      </div>
+      <div className="mt-8 w-full h-72 relative">
+        <Image
+          src={COVID_IMAGE_OUTDOORS}
+          alt="Women wearing a mask and walking on the street."
+          layout="fill"
+          className="object-cover object-center rounded-2xl"
+        />
+      </div>
     </div>
   );
 }
