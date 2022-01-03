1d6b210a700faea41fa16a6d9399c3d57c693781
Sparsh Paliwal <sparshpaliwal123@gmail.com>
2022-01-02 22:15:48 -0500

    Homepage: Add visualisation and iFrame

--- components/HomePage/VisualizationContainer.js
+++ components/HomePage/VisualizationContainer.js
@@ -1,7 +1,11 @@
 function VisualizationContainer() {
   return (
-    <div className="mt-36 w-full flex flex-wrap flex-col">
+    <div className="mt-24 w-full flex flex-wrap flex-col">
       <h1 className="text-4xl w-full font-serif italic">Visualization</h1>
+      <iframe
+        src="https://public.tableau.com/views/final_draft1/Story1?:language=en-US&publish=yes&:display_count=n&:origin=viz_share_link?:showVizHome=no&:embed=true"
+        className="mt-8 w-full h-[100vh]"
+      ></iframe>
     </div>
   );
 }
